8bf2d79ac67ea35738cb3761845e017ee9d4bdc2
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2024-10-31 14:54:51 -0300

    feat(pkg): remove comment

--- changelogPreset.config.js
+++ changelogPreset.config.js
@@ -1,6 +1,8 @@
-module.exports = {
-    name: "conventional-changelog-conventionalcommits",
-    header: '<div align="center"><h1>📝 Changelog</h1><p>All changes in this package are documented below.</p></div>\n\n---\n',
+const config = require('conventional-changelog-conventionalcommits')
+
+module.exports = config({
+    //name:"conventionalcommits",
+    //header: '<div align="center"><h1>📝 Changelog</h1><p>All changes in this package are documented below.</p></div>\n\n---\n',
     types: [
         { type: "feat", section: "✨ Features", hidden: false },
         { type: "fix", section: "🐛 Bug Fixes", hidden: false },
@@ -9,15 +11,11 @@ module.exports = {
         { type: "refactor", section: "♻️ Refactors", hidden: false },
         { type: "test", section: "🧪 Tests", hidden: false },
         { type: "build", section: "🛠 Build", hidden: true },
+        { type: "infra", section: "Infrastructure Changes", hidden: true },
         { type: "perf", hidden: true },
         { type: "style", hidden: true },
         { type: "ci", hidden: true },
         { type: "revert", hidden: true }
     ],
-    commitUrlFormat: '{{host}}/{{owner}}/{{repository}}/commit/{{hash}}',
-    compareUrlFormat: '{{host}}/{{owner}}/{{repository}}/compare/{{previousTag}}...{{currentTag}}',
-    issueUrlFormat: '{{host}}/{{owner}}/{{repository}}/issues/{{id}}',
-    userUrlFormat: '{{host}}/{{user}}',
-    releaseCommitMessageFormat: 'chore(release): {{currentTag}}',
-    issuePrefixes: ['#']
-};
+    //issuePrefixes: ['#']
+})
